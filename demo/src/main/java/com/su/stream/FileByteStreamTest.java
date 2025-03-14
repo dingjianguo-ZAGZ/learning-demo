9e2045d802607e16c16eab308f46cdd1e751eb28
苏红润 <dingjianguo66@163.com>
2025-03-14 19:06:50 +0800

    使用Serializable实现Externalizable测试

---
+++ demo/src/main/java/com/su/stream/FileByteStreamTest.java
@@ -0,0 +1,12 @@
+package com.su.stream;
+
+import java.io.File;
+
+public class FileByteStreamTest {
+    public void write(String fileName){
+        //创建文件对象
+        File file = new File(fileName);
+        //创建文件字节输出流
+
+    }
+}
